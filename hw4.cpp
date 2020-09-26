83344546489631319d7f25aa5623cd8fabfb79c3
Jeremias Celidon <jec222@txstate.edu>
2020-09-26 05:40:28 +0000

    testing

--- hw4.cpp
+++ hw4.cpp
@@ -38,6 +38,7 @@ list_t reverse(list_t list){
 
   return reverse_helper(list, list_make());
 }
+
 list_t append(list_t first, list_t second){
 
   if (list_isEmpty(first))
@@ -47,5 +48,55 @@ list_t append(list_t first, list_t second){
       /*
     // EFFECTS: returns the list (first second)
     */
+}
+static bool odd(int x){
+   if(x&&1)
+       return true;
+   else
+       return false;
+}
+
+list_t filter(list_t list, bool (*fn)(int)){
+
+    list_t l=list_make();
+   list_t f=list_make();
+   int x;
+   while(!list_isEmpty(list)){
+      
+       x=list_first(list);
+       list=list_rest(list);
+       if(fn(x))
+           l=list_make(x,l);
+   }
+
+   while(!list_isEmpty(l)){
+  
+       x=list_first(l);
+       f=list_make(x,f);
+       l=list_rest(l);
+   }
+
+
+   return f;
+
+    /*
+    // EFFECTS: returns a list containing precisely the elements of list
+    //          for which the predicate fn() evaluates to true, in the
+    //          order in which  they appeared in list.
+    */
+}
+list_t filter_odd(list_t list){
+  
+     list_t f=filter(list,odd);
+
+   return f;
+      /*
+    // EFFECTS: returns a new list containing only the elements of the
+    //          original list which are odd in value, 
+    //          in the order in which they appeared in list.
+    //
+    // For example, if you applied filter_odd to the list ( 4 1 3 0 )
+    // you would get the list ( 1 3 )
+    */ 
 }
 
